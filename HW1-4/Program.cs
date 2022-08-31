5bf20c844ae68b0a0d718571f99372ba4a222828
«Antonina <a1523@mail.ru>
2022-08-31 21:38:15 +0300

    HW1-4

---
+++ HW1-4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int startNumber = 1;
+
+Console.WriteLine($"Четные числа до {number}: ");
+
+while (startNumber <= number) {
+    if (startNumber % 2 == 0)
+    {
+        Console.WriteLine(startNumber);
+    }
+        startNumber = startNumber + 1;
+}
+
+
