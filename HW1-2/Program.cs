6e5902f3f1408902a209384d555518b1641a9139
«Antonina <a1523@mail.ru>
2022-08-31 21:33:58 +0300

    Add HW1-2

---
+++ HW1-2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число: ");
+int numb1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numb2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int numb3 = int.Parse(Console.ReadLine());
+
+int max = numb1;
+
+if (numb2 > max)
+{
+    max = numb2;
+}
+
+if (numb3 > max)
+{
+    max = numb3;
+}
+
+Console.WriteLine($"Максимальное число {max}");
+
+
